6ef58cb8b88672a0f16d965166f9a95a0644fe37
Asefy <simonbernard98@gmail.com>
2021-02-06 16:00:07 +0100

    J computation dynamic

--- section2.py
+++ section2.py
@@ -2,34 +2,34 @@ import numpy as np
 import math
 from section1 import *
 
-def expected_ret_det(g, U, x, policy, gamma, N):
+def expected_ret_det(g, U, x, policy, gamma, J_prev):
+
+	u = policy(U)
+	return R(g, f_det(x, u, g.shape)) + gamma * J_prev[f_det(x, u, g.shape)]
+
+
+def expected_ret_stoch(g, U, x, policy, gamma, J_prev):
 	if N <= 0:
 		return 0
 
 	else:
 		u = policy(U)
-		return R(g, f_det(x, u, g.shape)) + gamma * expected_ret_det(g, U, f_det(x, u, g.shape), policy, gamma, N-1)
-
-
-def expected_ret_stoch(g, U, x, policy, gamma, N):
-	if N <= 0:
-		return 0
-
-	else:
-		u = policy(U)
-		J_x_success = R(g, f_det(x, u, g.shape)) + gamma * expected_ret_det(g, U, f_det(x, u, g.shape), policy, gamma, N-1)
-		J_x_teleport =  R(g, (0,0)) + gamma * expected_ret_det(g, U, (0,0), policy, gamma, N-1)
+		J_x_success = R(g, f_det(x, u, g.shape)) + gamma * J_prev[f_det(x, u, g.shape)]
+		J_x_teleport =  R(g, (0,0)) + gamma * J_prev[0,0]
 		return (J_x_success + J_x_teleport)/2
 
 def compute_N(gamma, Br, thresh):
 	return math.ceil(math.log(thresh * (1-gamma) / Br , gamma))
 
-def compute_J(g, U, policy, gamma, N, expected_return):
+def compute_J_dyna(g, U, policy, gamma, N, expected_return):
 	J = np.zeros(g.shape)
 
-	for k in range(J.shape[0]):
-		for l in range(J.shape[1]):
-			J[k, l] = expected_return(g, U, (k, l), policy, gamma, N)
+	for t in range(1, N):
+		J_prev = J
+		for k in range(J.shape[0]):
+			for l in range(J.shape[1]):
+				J[k, l] = expected_return(g, U, (k, l), policy, gamma, J_prev)
+
 
 	return J
 
@@ -51,14 +51,7 @@ if __name__ == '__main__':
 
 	print("Chosen N : " + str(N))
 
-	"""
-	J_mu_x_det = expected_ret_det(g, U, x, policy_right, gamma, N)
-	J_mu_x_stoch = expected_ret_stoch(g, U, x, policy_right, gamma, N)
 
-	print(J_mu_x_det)
-	print(J_mu_x_stoch)
-	"""
-
-	J = compute_J(g, U, policy_right, gamma, N, expected_ret_det)
+	J = compute_J_dyna(g, U, policy_right, gamma, N, expected_ret_stoch)
 
 	print(J)
